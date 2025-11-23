826f3729c8444c9812134b48f6968586336912b3
clara5091 <168677884+clara5091@users.noreply.github.com>
2025-11-23 20:16:23 +0200

    Add role seeding during application startup
    Seed roles in the application at startup.

--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -38,4 +38,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await RoleSeeder.SeedRolesAsync(roleManager);
+}
+
 app.Run();
