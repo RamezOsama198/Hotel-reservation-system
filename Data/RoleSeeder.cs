f6ec25f15690dbc21b422ecb1068ae529af211f3
clara5091 <168677884+clara5091@users.noreply.github.com>
2025-11-23 20:11:57 +0200

    Add files via upload

---
+++ Data/RoleSeeder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HotelBooking.Data
+{
+    public static class RoleSeeder
+    {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = { "Admin", "Client", "Staff" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
